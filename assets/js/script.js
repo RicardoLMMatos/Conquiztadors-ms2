4f0ab48801315d544a78ef7d071497df96ed44b8
Ricardo Matos <ricardomatos_976@hotmail.com>
2023-03-16 18:39:43 +0000

    added an array of questions to the js file, WHOOP WHOOP

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+//Questions 
+
+let scienceQuestions = [ 
+     {    question: "What is the smallest particle in the universe?", 
+        choices: ["Atom", "Quark", "Electron", "Proton"],
+    answer: 1
+  },
+  {
+    question: "What is the name of the force that opposes motion between two surfaces in contact?",
+    choices: ["Momentum", "Inertia", "Friction", "Gravity"],
+    answer: 2
+  },
+  {
+    question: "What type of energy is associated with the motion of particles?",
+    choices: ["Potential energy", "Kinetic energy", "Chemical energy", "Thermal energy"],
+    answer: 1
+  },
+  {
+    question: "What is the name of the process by which plants release water vapor from their leaves?",
+    choices: ["Photosynthesis", "Transpiration", "Respiration", "Fermentation"],
+    answer: 2
+  },
+  {
+    question: "What is the name of the process by which a solid changes directly into a gas?",
+    choices: ["Evaporation", "Condensation", "Sublimation", "Deposition"],
+    answer: 3
+  },
+  {
+    question: "What is the name of the subatomic particle with a positive charge?",
+    choices: ["Electron", "Neutron", "Proton", "Photon"],
+    answer: 2
+  },
+  {
+    question: "What is the name of the phenomenon in which an electric current creates a magnetic field?",
+    choices: ["Electromagnetism", "Magnetization", "Induction", "Conduction"],
+    answer: 1
+  },
+  {
+    question: "What is the name of the force that causes an object to fall towards the ground?",
+    choices: ["Momentum", "Inertia", "Friction", "Gravity"],
+    answer: 4
+  },
+  {
+    question: "What is the name of the process by which light is refracted as it passes through a lens?",
+    choices: ["Diffraction", "Reflection", "Dispersion", "Refraction"],
+    answer: 4
+  },
+  {
+    question: "What is the name of the unit used to measure electrical resistance?",
+    choices: ["Ohm", "Watt", "Ampere", "Volt"],
+    answer: 1
+  }
+];
