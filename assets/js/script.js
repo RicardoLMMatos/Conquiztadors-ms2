70601c41cf1ee71121a5e0776f6738bf67004dd9
Ricardo Matos <ricardomatos_976@hotmail.com>
2023-03-20 18:14:53 +0000

    added classes and id's to some elements on questions pages to be used for the js quiz, made some css changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,124 @@
+$(document).ready(function(){
+// All coments were made by me to make me understand what is going on and to make sence of what I'm doing
+
+//Globals
+//The question heading
+const question = document.getElementById("question");
+//The answer list
+const answers = document.getElementById("answers");
+//The subject heading
+const subject = document.getElementById("subject");
+// The progress heading 
+const progress = document.getElementById("progress");
+// The total score heading
+const total = document.getElementById("total");
+// the progress heading 
+const restart = document.getElementById("restart")
+// The score
+let score = 0;
+
+// The question set variable
+let questionsSet = [];
+});
+
+
+
+
+
+
 //Questions 
 
-let scienceQuestions = [
-    {
-        question: "What is the name of the largest planet in our solar system?",
-        choice1: "Earth",
-        choice2: "Jupiter",
-        choice3: "Saturn",
-        choice4: "Uranus",
-        answer: 2,
-    },
+// let scienceQuestions = [
+//     {
+//         question: "What is the name of the largest planet in our solar system?",
+//         choice1: "Earth",
+//         choice2: "Jupiter",
+//         choice3: "Saturn",
+//         choice4: "Uranus",
+//         answer: 2,
+//     },
 
-    {
-        question: "What is the name of the largest ocean on Earth?",
-        choice1: "Atlantic",
-        choice2: "Pacific",
-        choice3: "Indian",
-        choice4: "Arctic",
-        answer: 2,
-    },
+//     {
+//         question: "What is the name of the largest ocean on Earth?",
+//         choice1: "Atlantic",
+//         choice2: "Pacific",
+//         choice3: "Indian",
+//         choice4: "Arctic",
+//         answer: 2,
+//     },
 
-    {
-        question: "What is the name of the largest country in the world?",
-        choice1: "Russia",
-        choice2: "Canada",
-        choice3: "China",
-        choice4: "United States",
-        answer: 1,
-    },
+//     {
+//         question: "What is the name of the largest country in the world?",
+//         choice1: "Russia",
+//         choice2: "Canada",
+//         choice3: "China",
+//         choice4: "United States",
+//         answer: 1,
+//     },
 
-    {   
-        question: "What is the name of the largest continent on Earth?",
-        choice1: "Asia",
-        choice2: "Africa",
-        choice3: "North America",
-        choice4: "Antarctica",
-        answer: 1,
-    },
+//     {   
+//         question: "What is the name of the largest continent on Earth?",
+//         choice1: "Asia",
+//         choice2: "Africa",
+//         choice3: "North America",
+//         choice4: "Antarctica",
+//         answer: 1,
+//     },
 
-    {
-        question: "What is the name of the largest animal on Earth?",
-        choice1: "Blue Whale",
-        choice2: "African Elephant",
-        choice3: "Giraffe",
-        choice4: "Hippopotamus",
-        answer: 1,
-    },
+//     {
+//         question: "What is the name of the largest animal on Earth?",
+//         choice1: "Blue Whale",
+//         choice2: "African Elephant",
+//         choice3: "Giraffe",
+//         choice4: "Hippopotamus",
+//         answer: 1,
+//     },
     
-{
-    question: "What is the name of the largest planet in our solar system?",
-    choice1: "Earth",
-    choice2: "Jupiter",
-    choice3: "Saturn",
-    choice4: "Uranus",
-    answer: 2,
-    },
+// {
+//     question: "What is the name of the largest planet in our solar system?",
+//     choice1: "Earth",
+//     choice2: "Jupiter",
+//     choice3: "Saturn",
+//     choice4: "Uranus",
+//     answer: 2,
+//     },
 
-    {
-    question: "What is the name of the largest ocean on Earth?",
-    choice1: "Atlantic",
-    choice2: "Pacific",
-    choice3: "Indian",
-    choice4: "Arctic",
-    answer: 2,
+//     {
+//     question: "What is the name of the largest ocean on Earth?",
+//     choice1: "Atlantic",
+//     choice2: "Pacific",
+//     choice3: "Indian",
+//     choice4: "Arctic",
+//     answer: 2,
 
-    },
+//     },
 
-    {   
-    question: "What is the name of the largest continent on Earth?",
-    choice1: "Asia",
-    choice2: "Africa",
-    choice3: "North America",
-    choice4: "Antarctica",
-    answer: 1,
+//     {   
+//     question: "What is the name of the largest continent on Earth?",
+//     choice1: "Asia",
+//     choice2: "Africa",
+//     choice3: "North America",
+//     choice4: "Antarctica",
+//     answer: 1,
 
-    },
+//     },
 
-    {
-    question: "What is the name of the largest animal on Earth?",
-    choice1: "Blue Whale",
-    choice2: "African Elephant",
-    choice3: "Giraffe",
-    choice4: "Hippopotamus",
-    answer: 1,
-    },  
+//     {
+//     question: "What is the name of the largest animal on Earth?",
+//     choice1: "Blue Whale",
+//     choice2: "African Elephant",
+//     choice3: "Giraffe",
+//     choice4: "Hippopotamus",
+//     answer: 1,
+//     },  
 
-    {
-    question: "What is the name of the largest planet in our solar system?",
-    choice1: "Earth",
-    choice2: "Jupiter",
-    choice3: "Saturn",
-    choice4: "Uranus",
-    answer: 2,
-    },
+//     {
+//     question: "What is the name of the largest planet in our solar system?",
+//     choice1: "Earth",
+//     choice2: "Jupiter",
+//     choice3: "Saturn",
+//     choice4: "Uranus",
+//     answer: 2,
+//     },
 
-];
+// ];
